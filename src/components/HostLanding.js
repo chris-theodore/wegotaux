37ab948142f65628e76bd0ec4775a13d0990a6d6
Chris Theodore <christopher.theodore@duke.edu>
2021-11-07 23:11:00 -0500

    frontend updates

--- src/components/HostLanding.js
+++ src/components/HostLanding.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { useHistory } from "react-router-dom";
+import { ExitOutline, PauseOutline, PlaySkipBackOutline, PlaySkipForwardOutline, MenuOutline, PeopleOutline, InformationCircleOutline } from 'react-ionicons'
 import '../styles/HostLanding.css' // CSS imported
 
 // Javascript Zone
@@ -7,11 +8,53 @@ import '../styles/HostLanding.css' // CSS imported
 
 // HTML Zone 
 export default function HostLanding() {
-    
-    return (
-        <section id="host-landing">
-            
+   const history = useHistory();
 
+    function handleSubmit(direction){
+        if(direction === "queue"){
+            history.push("queue");
+        } else if(direction === "listeners"){
+            history.push("listeners");
+        } else if(direction === "details"){
+            history.push("details");
+        } else if(direction === "leave"){
+            history.push("/");
+            // need to do an api call here to remove the user from the database.
+        } 
+    }
+
+
+    return (
+        <section id="listener-landing">
+            <div id="player">
+                <img id="album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                <div id="player-actions">
+                    <PlaySkipBackOutline class="p-action" color={'#00000'} title={"back"} height="25px" width="25px"/>
+                    <PauseOutline  class="p-action" color={'#00000'} title={"pause"} height="25px" width="25px"/>
+                    <PlaySkipForwardOutline  class="p-action" color={'#00000'} title={"forwards"} height="25px" width="25px"/>
+                </div>
+            </div>
+
+            <div id="user-actions">
+                <div class="u-action" onClick={() => handleSubmit("queue")}>
+                    <MenuOutline class="a-icon" color={'#00000'} title={"view-queue"} height="25px" width="25px"/>
+                    <p>View Queue</p>
+                </div>
+                <div class="u-action" onClick={() => handleSubmit("listeners")}>
+                    <PeopleOutline color={'#00000'} title={"view-listeners"} height="25px" width="25px"/>
+                    <p>Listeners</p>
+                </div>
+                <div class="u-action" onClick={() => handleSubmit("details")}>
+                    <InformationCircleOutline color={'#00000'}  title={"party-details"} height="25px" width="25px"/>                    
+                    <p>Party Details</p>
+                </div>
+                <div class="u-action" onClick={() => handleSubmit("leave")}>
+                <ExitOutline color={'#00000'}  title={"exit"} height="25px" width="25px"
+/>
+
+                    <p>Leave Party</p>
+                </div>
+            </div>
         </section>
     );
 }
--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,5 +1,8 @@
 import React from "react";
 import '../styles/Queue.css' // CSS imported
+import { ArrowBackCircleOutline, ArrowUpCircleOutline,ArrowDownCircleOutline } from 'react-ionicons'
+import { useHistory } from "react-router-dom";
+
 
 // Javascript Zone
 
@@ -7,9 +10,90 @@ import '../styles/Queue.css' // CSS imported
 // HTML Zone 
 export default function Queue() {
 
+    const history = useHistory();
+
+    function handlePageChange(direction){
+        if(direction === "back"){
+            history.push("listener");
+        } else if(direction === "q") {
+            history.push("search");
+        }
+    }
+
+    function handleVote(direction) {
+        if(direction === "up"){
+            // use react useState hook to increment votes and push to db
+            // make sure to connect to the given song can use dom stuff to retrieve song name/id
+            console.log("vote +1");
+        } else if(direction === "down"){
+            // use react useState hook to decrement votes and push to db 
+            // make sure to connect to the given song can use dom stuff to retrieve song name/id
+            console.log("vote -1");
+        }
+    }
+
+    
+    // gonna need to add the songs to song view with a for loop later from database, place holders for now.
+    // gonna need to add the songs to polls div with a for loop later from database, place holders for now.
+    // polls section would probs be scrollable later
     return (
         <section id="queue">
-           
+           <div id="header">
+                <ArrowBackCircleOutline onClick={() => handlePageChange("back")}color={'#00000'}  title={"back"} height="40px" width="40px"/>
+                <button id="q-song" onClick={() => handlePageChange("q")}>Queue Song</button>
+           </div>
+           <div id="song-view">
+               <div class="song-card">
+                    <img class="sc-album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                    <p>Song Name #1</p>
+               </div>
+               <div class="song-card">
+                    <img class="sc-album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                    <p>Song Name #2</p>
+               </div>
+           </div>
+           <div id="polls">
+                <div class="vote-card">
+                    <img class="voter-album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                    <p>Song Name</p>
+                    <div class="vote-tools">
+                        <ArrowUpCircleOutline onClick={() => handleVote("up")} color={'#00000'}  title={"upvote"} height="25px" width="25px"/>
+                        <ArrowDownCircleOutline onClick={() => handleVote("down")} color={'#00000'}  title={"downvote"} height="25px" width="25px"/>
+                    </div>
+               </div>
+               <div class="vote-card">
+                    <img class="voter-album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                    <p>Song Name</p>
+                    <div class="vote-tools">
+                        <ArrowUpCircleOutline onClick={() => handleVote("up")} color={'#00000'}  title={"upvote"} height="25px" width="25px"/>
+                        <ArrowDownCircleOutline onClick={() => handleVote("down")} color={'#00000'}  title={"downvote"} height="25px" width="25px"/>
+                    </div>
+               </div>
+               <div class="vote-card">
+                    <img class="voter-album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                    <p>Song Name</p>
+                    <div class="vote-tools">
+                        <ArrowUpCircleOutline onClick={() => handleVote("up")} color={'#00000'}  title={"upvote"} height="25px" width="25px"/>
+                        <ArrowDownCircleOutline onClick={() => handleVote("down")} color={'#00000'}  title={"downvote"} height="25px" width="25px"/>
+                    </div>
+                </div>
+               <div class="vote-card">
+                    <img class="voter-album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                    <p>Song Name</p>
+                    <div class="vote-tools">
+                        <ArrowUpCircleOutline onClick={() => handleVote("up")} color={'#00000'}  title={"upvote"} height="25px" width="25px"/>
+                        <ArrowDownCircleOutline onClick={() => handleVote("down")} color={'#00000'}  title={"downvote"} height="25px" width="25px"/>
+                    </div>
+                </div>
+               <div class="vote-card">
+                    <img class="voter-album-art" src="../data/images/albumcover-placeholder.jpg"/>
+                    <p>Song Name</p>
+                    <div class="vote-tools">
+                        <ArrowUpCircleOutline onClick={() => handleVote("up")} color={'#00000'}  title={"upvote"} height="25px" width="25px"/>
+                        <ArrowDownCircleOutline onClick={() => handleVote("down")} color={'#00000'}  title={"downvote"} height="25px" width="25px"/>
+                    </div>
+        </div>
+           </div>
 
         </section>
     );
