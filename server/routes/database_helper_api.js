eb0b8d84660c0bd76c6e0c2505665ee0558bca9f
cps52 <cps52@duke.edu>
2021-12-01 22:52:28 -0500

    Update database_helper_api.js

--- server/routes/database_helper_api.js
+++ server/routes/database_helper_api.js
@@ -11,16 +11,14 @@ const Listening_Party = function(spotify_playlist_name, time_created, currently_
   this.device_id = device_id;
   this.spotify_user_id = spotify_user_id;
   this.playlist_id = playlist_id;
-  
 }
 
-const Voting_Record = function(fun_name, id, vote, vote_time, spotify_id, time_added){
+const Voting_Record = function(fun_name, id, vote, vote_time, song_id){
   this.fun_name = fun_name;
   this.id = id;
   this.vote = vote;
   this.vote_time = vote_time;
-  this.spotify_id = spotify_id;
-  this.time_added = time_added;
+  this.song_id = song_id;
 }
 
 
@@ -44,17 +42,10 @@ const Song = function(spotify_id, party_id, song_id, song_length, is_removed, pl
 
 
 const db_client = {
-  //CREATE 
-  //(spotify_playlist_name, time_created, currently_playing, device_id, spotify_user_id, playlist_id, id)
-  // this.spotify_playlist_name = spotify_playlist_name;
-  // this.id = id;
-  // this.time_created = time_created;
-  // this.currently_playing = currently_playing;
-  // this.device_id = device_id;
-  // this.spotify_user_id = spotify_user_id;
-  // this.playlist_id = playlist_id;
+
+ //CREATE
   Listening_Party_Create: (pname, did, uid, pid, id, result) => {
-    const newParty = new Listening_Party(pname, new Date().toISOString().slice(0, 19).replace('T', ' '), 1, did, uid, pid, id)
+    const newParty = new Listening_Party(pname, new Date().toISOString().slice(0, 19).replace('T', ' '), 1, did, uid, pid, parseInt(id))
     connection.query("INSERT INTO Listening_Party SET ?", newParty, (err, res) =>{
       if (err) {
         console.log("error: ", err);
@@ -79,86 +70,109 @@ const db_client = {
     //result(null, { id: result.id, ...newUser });
   });
 },
-Voting_Record_Create: (fun_name, id, vote, spotify_id, time_added) => {
-  const newRecord = new Voting_Record(fun_name, id, vote, new Date().toISOString().slice(0, 19).replace('T', ' '), spotify_id, time_added)
+Voting_Record_Create: (fun_name, id, vote, song_id) => {
+  const newRecord = new Voting_Record(fun_name, id, vote, new Date().toISOString().slice(0, 19).replace('T', ' '), song_id)
   connection.query("INSERT INTO Voting_Record SET ?", newRecord, (err, result) =>{
+    if (err) {
+      console.log("error: ", err);
+      return err;
+    }
+    console.log("created voting record: ", { ...newRecord });
+    return result;
+  });
+},
+Song_Create: async (spotify_id, party_id, song_length) => {
+  let new_sid = uuidv4();
+  await connection.query("SELECT COALESCE(MAX(playlist_position),0) AS value FROM Song WHERE party_id = "+parseInt(party_id)+"", (err, result) =>{
     if (err) {
       console.log("error: ", err);
       //result(err, null);
       return err;
     }
-    console.log("created user: ", { ...newRecord });
-    return result;
-    //result(null, { id: result.id, ...newRecord });
+    console.log(result[0].value);
+    const newSong = new Song(spotify_id, party_id, new_sid, song_length, 0, result[0].value);
+    connection.query("INSERT INTO Song SET ?", newSong, (err, result) =>{
+      if (err) {
+        console.log("error: ", err);
+        return err;
+      }
+      console.log("created song: ", { ...newSong });
+      console.log("here1");
+      //return song_id;
+      return newSong.song_id;
+    });
+    console.log("here2");
+    return newSong.song_id;
   });
-},
-Song_Create: (spotify_id, party_id, song_length) => {
-  let position;
-  connection.query("SELECT COALESCE(MAX(playlist_position),0) AS value FROM Song WHERE party_id = party_id") //BUG HERE//
-  .then(position => {
-    const newSong = new Song(spotify_id, party_id, uuidv4(), song_length, 0, position);
-  connection.query("INSERT INTO Song SET ?", newSong, (err, result) =>{
-    if (err) {
-      console.log("error: ", err);
-      return err;
-    }
-    console.log("created song: ", { ...newSong });
-    return result;
-  });
-  });
-  
-  console.log(position);
-  
+  return new_sid;
 },
 
 //READ
 Listening_Party_Read: (id) => {
-  connection.query("SELECT * FROM `Listening_Party` WHERE `id` = '"+id+"')", (err, result) =>{
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    console.log(result);
-    result(null, result);
+   return new Promise((resolve, reject) => {
+     connection.query("SELECT * FROM Listening_Party WHERE id = ?", [parseInt(id)], async (err, result) =>{
+      if (err) {
+        return reject(err);
+      }
+      return resolve(result[0]);
+  });
 });
 },
+
 User_Read: (fun_name, id) => {
-  connection.query("SELECT * FROM `Users` WHERE `id` = '"+id+" and  `fun_name` = '"+fun_name+"')", (err, result) =>{
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    console.log(result);
-    result(null, result);
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM Users WHERE fun_name = ? and id = ?", [fun_name, parseInt(id)], async (err, result) =>{
+     if (err) {
+       return reject(err);
+     }
+     return resolve(result[0]);
+ });
 });
 },
-Song_Read: (id, spotify_id, time_added) => {
-  connection.query("SELECT * FROM `Song` WHERE `id` = '"+id+" and  `spotify_id` = '"+spotify_id+"' and  `time_added` = '"+time_added+"')", (err, result) =>{
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    console.log(result);
-    result(null, result);
+
+Song_Read: (song_id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM Song WHERE song_id = ?", [song_id], async (err, result) =>{
+     if (err) {
+       return reject(err);
+     }
+     return resolve(result[0]);
+ });
+});  
+},
+
+//SINGLE VOTING RECORD READ
+Voting_Read: (fun_name, id, song_id) => {
+  return new Promise((resolve, reject) => {
+    //and id = ? and song_id = ?
+    //, parseInt(id), song_id
+    connection.query("SELECT * FROM Voting_Record WHERE fun_name = ? and id = ? and song_id = ?", [fun_name, parseInt(id), song_id], async (err, result) =>{
+     if (err) {
+       return reject(err);
+     }
+     console.log(result);
+     return resolve(result[0]);
+ });
 });
 },
-Voting_Read: (fun_name, id, spotify_id, time_added) => {
-  connection.query("SELECT * FROM `Voting_Record` WHERE  `fun_name` = '"+fun_name+" and `id` = '"+id+" and  `spotify_id` = '"+spotify_id+"' and  `time_added` = '"+time_added+"')", (err, result) =>{
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    console.log(result);
-    result(null, result);
+
+//VOTING BLOCK READ
+Voting_Block_Read: (id) => {
+  return new Promise((resolve, reject) => {
+   
+    connection.query("SELECT * FROM Voting_Record WHERE fun_name = ? and id = ? and song_id = ?", [parseInt(id)], async (err, result) =>{
+     if (err) {
+       return reject(err);
+     }
+     console.log(result);
+     return resolve(result[0]);
+ });
 });
 },
+
 //UPDATE
 Listening_Party_Update_Curr_Playing: (id) => {
-  connection.query("UPDATE Listening_Party SET currently_playing = currently_playing + 1 WHERE id = ?", [id], (err, result) =>{
+  connection.query("UPDATE Listening_Party SET currently_playing = currently_playing + 1 WHERE id = ?", [parseInt(id)], (err, result) =>{
     if (err) {
       console.log("error: ", err);
       return err;
@@ -167,53 +181,87 @@ Listening_Party_Update_Curr_Playing: (id) => {
     return result;
 });
 },
-Song_Update_Time_Removed: (spotify_id, id, time_added) => {
-  connection.query("UPDATE Song SET time_removed = ? WHERE spotify_id = ? and id = ? and time_added = ?", [new Date().toISOString().slice(0, 19).replace('T', ' '), spotify_id, id, time_added], (err, result) =>{
+Song_Update_Removed: (song_id) => {
+  connection.query("UPDATE Song SET is_removed = ? WHERE song_id = ?", [1, song_id], (err, result) =>{
     if (err) {
       console.log("error: ", err);
-      result(err, null);
-      return;
+      return err;
     }
-    console.log("updated song");
-    result(null, result);
+    return result;
 });
 },
-Voting_Time_Removed: (spotify_id, id, time_added) => {
-  connection.query("UPDATE Song SET time_removed = ? WHERE spotify_id = ? and id = ? and time_added = ?", [new Date().toISOString().slice(0, 19).replace('T', ' '), spotify_id, id, time_added], (err, result) =>{
+Voting_Record_Update_Vote: (vote, fun_name, id, song_id) => {
+  connection.query("UPDATE Voting_Record SET vote = ?, vote_time = ? WHERE fun_name = ? and id = ? and song_id = ?", [vote, new Date().toISOString().slice(0, 19).replace('T', ' '), fun_name, parseInt(id), song_id], (err, result) =>{
     if (err) {
       console.log("error: ", err);
-      result(err, null);
-      return;
+      return err;
     }
-    console.log("updated song");
-    result(null, result);
-});
-},
-//EXTRA
-Listening_Party_Lookup: (id) => {
-  connection.query("SELECT EXISTS( SELECT 1 FROM `Listening_Party` WHERE `id` = '"+id+"')", (err, result) =>{
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    console.log(result);
+    console.log("updated voting record");
+    return result;
 });
 },
 
+//DELETE 
+Listening_Party_Delete: (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("DELETE FROM Listening_Party WHERE id = ?", [parseInt(id)], async (err, result) =>{
+     if (err) {
+       return reject(err);
+     }
+     return resolve(result[0]);
+ });
+});  
+},
+User_Delete_Indiv: (fun_name, id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("DELETE FROM Users WHERE fun_name = ? and id = ?", [fun_name, parseInt(id)], async (err, result) =>{
+     if (err) {
+       return reject(err);
+     }
+     return resolve(result[0]);
+ });
+});  
+},
+Song_Delete_Indiv: (song_id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("DELETE FROM Song WHERE song_id = ?", [song_id], async (err, result) =>{
+     if (err) {
+       return reject(err);
+     }
+     return resolve(result[0]);
+ });
+});  
+},
+
+//EXTRA
+Listening_Party_Lookup: (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT EXISTS( SELECT 1 FROM `Listening_Party` WHERE `id` = '"+id+"')", async (err, result) =>{
+     if (err) {
+       //console.log("error: ", err);
+       return reject(err);
+     }
+     //console.log(result[0]);
+   
+     return resolve(result[0]);
+ });
+});
+},
+Generate_Voting_Block: (id) => {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT Voting_Record.song_id, SUM(Voting_Record.vote) AS total_votes FROM Voting_Record, Song WHERE Voting_Record.id = ? GROUP BY Voting_Record.song_id", [parseInt(id)], async (err, result) =>{
+     if (err) {
+       //console.log("error: ", err);
+       return reject(err);
+     }
+     console.log(result);
+     return resolve(result[0]);
+ });
+});
+}
 };
 
 
-  function Find_Playlist_Position (id) {
-    connection.query("SELECT COALESCE(MAX(playlist_position),0) AS value FROM Song WHERE party_id = "+parseInt(id)+"", (err, result) => {
-      if (err) {
-        console.log("error: ", err);
-        return err;
-      }
-      //console.log(result[0].value);
-      return result[0].value;
-    });
-  }
 
 
 
